677bd37e170c3c6a312a48872cbe1b408d867a0d
Vadym_Samardak <fatalerror14.88@icloud.com>
2025-05-25 14:04:09 +0200

    prepared for coding

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-`use strict`;
-const allItems = document.querySelectorAll(".item")
-console.log(`Number of categories: ${allItems.length}`);
-allItems.forEach(item => {
-    console.log(`Category: ${item.querySelector("h2").textContent}`)
-    console.log(`Elements: ${item.querySelectorAll("li").length}`)
-});
--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-`use strict`;
-const textInput = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-
-textInput.addEventListener('input', (event) => {
-    event.currentTarget.value.trim().length === 0
-    ? (output.textContent = 'Anonymous')
-    : (output.textContent = event.currentTarget.value.trim());
-});
